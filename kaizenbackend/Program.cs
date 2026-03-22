71794cd0c52b86a88ad92fdb74deab41bc7a01cf
Tifany <kosgeitiffany@gmail.com>
2026-03-22 13:14:22 +0300

    backend running, CORS configured, Swagger working

--- kaizenbackend/Program.cs
+++ kaizenbackend/Program.cs
@@ -2,6 +2,17 @@ using Microsoft.EntityFrameworkCore;
 using backend.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy.WithOrigins("http://localhost:5173"
+                          ,"https://localhost:5173"
+                          )
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
 
 // Add services to the container
 builder.Services.AddControllers();
@@ -13,13 +24,13 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
+app.UseCors("AllowFrontend");
 
 // Configure the HTTP request pipeline
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseHttpsRedirection();
 
